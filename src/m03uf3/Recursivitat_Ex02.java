eecb600f80dcb3d534ee2a6ad994d7fe21443784
Roberto Ferrero <roberto.ferrero.pintor@gmail.com>
2023-05-23 09:03:27 +0200

    Factorial recursiu

--- src/m03uf3/Recursivitat_Ex02.java
+++ src/m03uf3/Recursivitat_Ex02.java
@@ -1,3 +1,5 @@
+package m03uf3;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
